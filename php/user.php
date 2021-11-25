bbfe269e1acb8b0aa0b69294c0f12e25a6f0ef51
yasmeen-1989 <jabi.yasmeen@gmail.com>
2021-11-25 18:39:28 -0500

    Update user.php

--- php/user.php
+++ php/user.php
@@ -6,6 +6,38 @@
        echo $user['Username'] . "<br>";
      }
     include('user.html');
+
+    //Query the seeds for the user,
+    $seedSelect = "SELECT * FROM BC_Seed";
+
+
+
+    $resQuery = mysqli_query($dblink, $seedSelect);
+
+    if (!$resQuery)
+    {
+        echo "<p>!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" . mysqli_error($dblink) . "</p>";
+    }
+
+
+    echo "<table border = '1'>";
+    echo "<tr><td>ID</td><td>SeedName</td><td>Description</td><td>Quantity</td><td>PlantTypeID</td><td>LifeCycleID</td><td>SunID</td><td>MaintenanceID</td></tr>";
+    while($rowData = mysqli_fetch_assoc($resQuery))
+    {
+      echo "<tr><td>{$rowData['SeedName']}</td><td>{$rowData['Description']}</td><td>{$rowData['Quantity']}</td><td>{$rowData['PlantTypeID']}</td><td>{$rowData['LifeCycleID']}</td><td>{$rowData['SunID']}</td><td>{$rowData['MaintenanceID']}</td></tr>";
+    }
+    echo "</table>";
+
+
+
+
+
+
+    //$seedsData = exeSelect($dblink, $seedSelect);
+    //echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+    //var_dump($seedsData);
+    //echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+
 ?>
 
 <?php
