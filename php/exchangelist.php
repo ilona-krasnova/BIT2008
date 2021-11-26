0ef761a22aeb9cf77f42071ca246462a99a99eb7
morganmdg <92071955+morganmdg@users.noreply.github.com>
2021-11-26 01:19:39 -0500

    Seeds + Exchange List

--- php/exchangelist.php
+++ php/exchangelist.php
@@ -1,13 +1,28 @@
 <?php
+
     include('header.php');
 
-    $users = exeSelect($dblink, "SELECT ID, Username, Email, PasswordHash FROM BC_User");
-    foreach ($users as $user) {
-       echo $user['Username'] . "<br>";
-     }
-    include('exchangelist.html');
-?>
+    //SQL to get Username and SeedName from database
+    $name = exeSelect($dblink, "SELECT BC_User.Username, BC_Seed.SeedName
+      FROM BC_User INNER JOIN BC_Seed ON BC_User.ID = BC_Seed.UserID");
 
-<?php
+    //SQL to get Exchange ID and Requested from database
+    $info = exeSelect($dblink, "SELECT BC_Exchange.ID, BC_Request.Requested
+      FROM BC_Seed, BC_Request INNER JOIN BC_Exchange ON BC_Request.ID = BC_Exchange.RequestID");
+
+    // debugging
+
+    //Printing off Username and Seedname
+    foreach ($name as $row) {
+          echo implode(" | ", $row) . "<br>";
+    }
+
+    //Printing off ID and Requested
+    foreach ($info as $rowData) {
+          echo implode(" | ", $rowData) . "<br>";
+    }
+
+
+    include('exchangelist.html');
     include('footer.php');
 ?>
