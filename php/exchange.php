a0ae7079f1ad78906708e672d2fca568a8911a1f
yasmeen-1989 <jabi.yasmeen@gmail.com>
2021-11-27 23:00:14 -0500

    Update exchange.php

--- php/exchange.php
+++ php/exchange.php
@@ -5,7 +5,32 @@
     foreach ($users as $user) {
        echo $user['Username'] . "<br>";
     }
-    include('exchange1.html');
+    include('exchange.html');
+
+    //User 4's seed is now user 3's seed
+    $seedSwap = "UPDATE BC_Seed SET UserID = 4 WHERE ID = 3 AND UserID = 3";
+
+    $resQuery = mysqli_query($dblink, $seedSwap);
+
+    if (!$resQuery)
+    {
+        echo "<p>" . mysqli_error($dblink) . "</p>";
+    }
+
+    //User 3's seed is now user 4's seed
+    $seedSwap = "UPDATE BC_Seed SET UserID = 3 WHERE ID = 4 AND UserID = 4";
+
+    $resQuery = mysqli_query($dblink, $seedSwap);
+
+    if (!$resQuery)
+    {
+      echo "<p>" . mysqli_error($dblink) . "</p>";
+    }
+
+
+    echo "success!";
+
+
 ?>
 
 <?php
