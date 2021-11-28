f39de893ad504efa58901558d24ab822a5befcef
yasmeen-1989 <jabi.yasmeen@gmail.com>
2021-11-27 23:00:24 -0500

    Update newseed.php

--- php/newseed.php
+++ php/newseed.php
@@ -7,10 +7,18 @@
      }
     include('newseed.html');
 
-    //Missing ID, UserID, PlantTypeID, LifeCycleID, SunID, MaintenanceID
+    //In part 4, will get user input instead of hard coded values to be added to table
     //$createNewSeed = "INSERT INTO BC_Seed (ID, UserID, SeedName, Description, Quantity, PlantTypeID, LifeCycleID, SunID, MaintenanceID) VALUES ('3', '3', 'Sunflower Seeds', 'Yellow petals, brown centre. Grows very big.', '5', '3', '1', '1', '1')";
     //$resQuery = mysqli_query($dblink, $createNewSeed);
-    //echo "hello world";
+
+    $createNewSeed = "INSERT INTO BC_Seed (ID, UserID, SeedName, Description, Quantity, PlantTypeID, LifeCycleID, SunID, MaintenanceID) VALUES ('4', '4', 'Pumpkin Seeds', 'Medium to large orange squash.', '4', '2', '1', '1', '2')";
+    $resQuery = mysqli_query($dblink, $createNewSeed);
+
+    //$createNewSeed = "INSERT INTO BC_Seed (ID, UserID, SeedName, Description, Quantity, PlantTypeID, LifeCycleID, SunID, MaintenanceID) VALUES ('5', '5', 'Peonies', 'Large bush with large pink flowers.', '1', '3', '2', '1', '2')";
+    //$resQuery = mysqli_query($dblink, $createNewSeed);
+
+    echo "Hello world";
+
 
 ?>
 
