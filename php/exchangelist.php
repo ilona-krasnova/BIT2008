288193b457921a8cac4461eef8b089f25b03e550
morganmdg <92071955+morganmdg@users.noreply.github.com>
2021-11-28 20:55:44 -0500

    Updated exchange list pages

--- php/exchangelist.php
+++ php/exchangelist.php
@@ -2,25 +2,19 @@
 
     include('header.php');
 
-    //SQL to get Username and SeedName from database
-    $name = exeSelect($dblink, "SELECT BC_User.Username, BC_Seed.SeedName
-      FROM BC_User INNER JOIN BC_Seed ON BC_User.ID = BC_Seed.UserID");
 
-    //SQL to get Exchange ID and Requested from database
-    $info = exeSelect($dblink, "SELECT BC_Exchange.ID, BC_Request.Requested
-      FROM BC_Seed, BC_Request INNER JOIN BC_Exchange ON BC_Request.ID = BC_Exchange.RequestID");
-
-    // debugging
-
-    //Printing off Username and Seedname
-    foreach ($name as $row) {
-          echo implode(" | ", $row) . "<br>";
+    //Printing off first requested seed name
+    $userRequestSeed = "SELECT BC_Seed.SeedName, BC_User.ID, BC_Request.Requested
+    FROM (BC_User INNER JOIN BC_Seed ON BC_User.ID = BC_Seed.UserID)
+    INNER JOIN BC_Request ON BC_User.ID = BC_Request.UserID";
+    $query = mysqli_query($dblink, $userRequestSeed);
+    $numRows = mysqli_num_rows($query);
+    if($numRows > 0) {
+      while ($rowData = mysqli_fetch_assoc ($query)) {
+        break;
+      }
     }
 
-    //Printing off ID and Requested
-    foreach ($info as $rowData) {
-          echo implode(" | ", $rowData) . "<br>";
-    }
 
 
     include('exchangelist.html');
