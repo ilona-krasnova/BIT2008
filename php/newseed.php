f2af7b415b42ef3fc25256a5999423c694d9ae97
yasmeen-1989 <jabi.yasmeen@gmail.com>
2021-12-12 11:34:40 -0500

    Update newseed.php

--- php/newseed.php
+++ php/newseed.php
@@ -7,9 +7,6 @@
   //   }
     include('newseed.html');
 
-    //In part 4, will get user input instead of hard coded values to be added to table
-    //$createNewSeed = "INSERT INTO BC_Seed (ID, UserID, SeedName, Description, Quantity, PlantTypeID, LifeCycleID, SunID, MaintenanceID) VALUES ('3', '3', 'Sunflower Seeds', 'Yellow petals, brown centre. Grows very big.', '5', '3', '1', '1', '1')";
-    //$resQuery = mysqli_query($dblink, $createNewSeed);
 
     $seed = getRequestParam('SeedName');
     $amount = getRequestParam('quantity');
