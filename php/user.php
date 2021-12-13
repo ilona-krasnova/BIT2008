9fb25d6cb48bf70191f08f38e1ec1d20c0e056b4
yasmeen-1989 <jabi.yasmeen@gmail.com>
2021-12-12 19:59:04 -0500

    Update user.php

--- php/user.php
+++ php/user.php
@@ -11,28 +11,29 @@
     $seedSelectSQL = "SELECT SeedName, Description, Quantity, PlantTypeID, LifeCycleID, SunID, MaintenanceID FROM BC_Seed WHERE UserID ="  .  $currUser;
     $seedSelectResult = mysqli_query($dblink, $seedSelectSQL);
 
-
+    echo "<div class='ui tabular menu'>
+        <div class='item active'>Seeds you have listed</div></div>";
+     echo "<table class='ui compact celled table'>";
+     echo "<thead>
+       <tr>
+         <th class='three wide'>Seed Name</th>
+         <th>Description</th>
+         <th>Quantity</th>
+         <th>Plant Type ID</th>
+         <th>Life Cycle ID</th>
+         <th>Sun ID</th>
+        <th>Maintenance ID</th>
+       </tr>
+     </thead>";
 
     foreach ($seedSelectResult as $z) {
-      echo "<div class='ui tabular menu'>
-          <div class='item active'>Seeds you have listed</div></div>";
-       echo "<table class='ui compact celled table'>";
-       echo "<thead>
-         <tr>
-           <th class='three wide'>Seed Name</th>
-           <th>Description</th>
-           <th>Quantity</th>
-           <th>Plant Type ID</th>
-           <th>Life Cycle ID</th>
-           <th>Sun ID</th>
-          <th>Maintenance ID</th>
-         </tr>
-       </thead>";
+
        echo "<td>";
        echo $z['SeedName'] .  "</td>"  . "<td>" . $z['Description'] . "</td>"  .  "<td>"  .  $z['Quantity']  .  "</td>" . "<td>"  .  $z['PlantTypeID'] .  "</td>"  .  "<td>"  .   $z['LifeCycleID']   .   "</td>"  .  "<td>"  .  $z['SunID']  .  "</td>"  .  "<td>"  .  $z['MaintenanceID']  .  "</tr>";
-       //echo "</td>";
-       echo "</table>";
+
      }
+
+     echo "</table>";
 
 ?>
 
